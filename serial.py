3f25912ff3af762b964dd7f20ec15bf05795186b
Jake Morris <jake.a.morris95@gmail.com>
2021-05-10 19:09:14 -0400

    serial.py solutions

--- serial.py
+++ serial.py
@@ -1,8 +1,9 @@
 """Python serial number generator."""
 
+
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -20,3 +21,19 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start, counter=0):
+        self.start = start
+        self.counter = counter
+
+    def __repr__(self):
+        return f"<SerialGenerator start={self.start}>"
+
+    def generate(self):
+        '''Returns a serial number and increments itself each time'''
+        number = self.start + self.counter
+        self.counter += 1
+        return number
+
+    def reset(self):
+        '''Resets the serial number back to start'''
+        self.counter = 0
